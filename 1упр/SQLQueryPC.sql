23b7147d877da4951611a4888beb65e27ed525d2
Anita Razpopova <62252172+AnitaRazpopova@users.noreply.github.com>
2021-06-08 14:06:12 +0300

    Update SQLQueryPC.sql

--- 1упр/SQLQueryPC.sql
+++ 1упр/SQLQueryPC.sql
@@ -1,20 +1,20 @@
-/*SELECT model, speed AS 'MHz', hd AS 'GB'
+SELECT model, speed AS 'MHz', hd AS 'GB'
 FROM laptop
-WHERE price < 1200;*/
+WHERE price < 1200;
 
-/*SELECT DISTINCT type
+SELECT DISTINCT type
 FROM printer
-where price>0;*/
+where price>0;
 
-/*SELECT model, ram, screen
+SELECT model, ram, screen
 FROM laptop
-WHERE price > 1000;*/
+WHERE price > 1000;
 
-/*SELECT *
+SELECT *
 FROM printer
-WHERE color='y';*/
+WHERE color='y';
 
-/*SELECT model, speed, hd, cd, price
+SELECT model, speed, hd, cd, price
 FROM PC
-WHERE cd = '12x' or cd = '16x' and price<2000;*/
+WHERE cd = '12x' or cd = '16x' and price<2000;
 
